8f3c33b16538c736b4d184dd927152d9e918206e
Python <ajaykumar6981@gmail.com>
2024-06-21 02:12:19 +0530

    ml complete

---
+++ Machine Learning/Part 4 - Clustering/Section 25 - Hierarchical Clustering/Python/hierarchical_clustering.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values
+
+
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
+y_hc = hc.fit_predict(X)
+
+
